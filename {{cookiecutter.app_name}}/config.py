e7c0227c9a7871b116185a4036ccb175b3d00107
jetilton <jfftilton@gmail.com>
2019-02-13 08:26:26 -0800

    initial commit

---
+++ {{cookiecutter.app_name}}/config.py
@@ -0,0 +1,23 @@
+import os
+from environs import Env
+
+
+env = Env()
+env.read_env()
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+		'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    ENV = env.str('FLASK_ENV', default='production')
+    DEBUG = ENV == 'development'
+    SQLALCHEMY_DATABASE_URI = env.str('DATABASE_URL')
+    SECRET_KEY = env.str('SECRET_KEY') or 'you-will-never-guess'
+    BCRYPT_LOG_ROUNDS = env.int('BCRYPT_LOG_ROUNDS', default=13)
+    DEBUG_TB_ENABLED = DEBUG
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+    CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/compat.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+text_type = str
+binary_type = bytes
+string_types = (str,)
+unicode = str
+basestring = (str, bytes)
