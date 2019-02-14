967cd63066d49c2fb3854e8018d502a812801276
jetilton <jfftilton@gmail.com>
2019-02-14 13:36:21 -0800

    fixed js block added dash link

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/user/views.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/user/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """User views."""
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 from flask_login import login_required
 
 blueprint = Blueprint('user', __name__, url_prefix='/users', static_folder='../static')
@@ -11,3 +11,9 @@ blueprint = Blueprint('user', __name__, url_prefix='/users', static_folder='../s
 def members():
     """List members."""
     return render_template('users/members.html')
+
+
+@blueprint.route('/dashboard/', methods=['GET', 'POST'])
+def dashboard():
+    """dashboard"""
+    return render_template('users/dashboard.html')
