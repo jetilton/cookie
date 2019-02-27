6b8c23417e5637e15bb7db86c64f4b18cb618ffe
jetilton <jfftilton@gmail.com>
2019-02-27 09:33:11 -0800

    changed environment, fix to js template

--- {{cookiecutter.app_name}}/config.py
+++ {{cookiecutter.app_name}}/config.py
@@ -1,22 +1,18 @@
 import os
-from environs import Env
+from dotenv import load_dotenv
 
 
-env = Env()
-env.read_env()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
 		'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    ENV = env.str('FLASK_ENV', default='production')
-    DEBUG = ENV == 'development'
-    SQLALCHEMY_DATABASE_URI = env.str('DATABASE_URL')
-    SECRET_KEY = env.str('SECRET_KEY') or 'you-will-never-guess'
-    BCRYPT_LOG_ROUNDS = env.int('BCRYPT_LOG_ROUNDS', default=13)
+    FLASK_ENV = os.environ.get('ENV')
+    DEBUG = FLASK_ENV == 'development'
+    BCRYPT_LOG_ROUNDS = os.environ.get('BCRYPT_LOG_ROUNDS')
     DEBUG_TB_ENABLED = DEBUG
     DEBUG_TB_INTERCEPT_REDIRECTS = False
     CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
